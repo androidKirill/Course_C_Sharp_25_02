32bd1bba0f82be9eaf7bd287b6423cb263c63b92
mary <mm@mail.ru>
2023-03-13 20:23:55 +0300

    Tasks

---
+++ Lesson_6/6_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+while (count != n)
+{
+    if (n < 0)    
+        Console.Write("Необходимо вводить положительные целые числа.");    
+    else
+    {
+        if (count % 2 == 0)
+            Console.Write(count + " ");
+    }
+    count++;
+}
