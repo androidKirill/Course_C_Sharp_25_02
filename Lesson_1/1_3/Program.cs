2166afae5351761e89c21f2ab7e7004cac604975
mary <mm@mail.ru>
2023-03-13 09:45:37 +0300

    Lessson 1 & 2

--- Lesson_1/1_3/Program.cs
+++ Lesson_1/1_3/Program.cs
@@ -16,13 +16,13 @@ while (m <= N)
 
 // --------------------------------------- вариант 2
 
-int N = int.Parse(Console.ReadLine()!);
-int m = -N;
+// int N = int.Parse(Console.ReadLine()!);
+// int m = -N;
 
-while (m != N)
-{
-    Console.Write($"{m} ");
-    if (N > 0) m += 1;
-    else m -= 1;
-}
-Console.Write($"{m} ");
+// while (m != N)
+// {
+//     Console.Write($"{m} ");
+//     if (N > 0) m += 1;
+//     else m -= 1;
+// }
+// Console.Write($"{m} ");
---
+++ Lesson_2/2_2/Program.cs
@@ -0,0 +1,59 @@
+﻿// int MaxNumber(int a)
+// {
+//     Console.WriteLine(a);
+
+//     if (a % 10 > a / 10)
+//         return a % 10;
+//     return a / 10;
+
+// }
+
+
+// Console.WriteLine(MaxNumber(new Random().Next(10, 100)));
+
+void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int indexOf(int[] collection, int find)
+{
+    int index = 0;
+    int count = collection.Length;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+array[4] = 4;
+array[6] = 4;
+int pos = indexOf(array, 1);
+Console.WriteLine();
+Console.WriteLine(pos);
